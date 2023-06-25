9cf43a7107cc198e74cc66fd9f050031b851e616
Ankit Anand <ankitanand1974@gmail.com>
2023-06-25 16:55:13 +0530

    Updates

---
+++ precache-manifest.83fabde74a3afeb80d909ac399bef39d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "8398adeec7ca0d32ef03",
+    "url": "/TrustShare/static/css/main.2b9b759b.chunk.css"
+  },
+  {
+    "revision": "8398adeec7ca0d32ef03",
+    "url": "/TrustShare/static/js/main.55645c2f.chunk.js"
+  },
+  {
+    "revision": "4118279a1a4ccaa6da73",
+    "url": "/TrustShare/static/js/runtime~main.7fbd5438.js"
+  },
+  {
+    "revision": "aeab388d65db118b9b74",
+    "url": "/TrustShare/static/css/2.37679456.chunk.css"
+  },
+  {
+    "revision": "aeab388d65db118b9b74",
+    "url": "/TrustShare/static/js/2.f05a6c3d.chunk.js"
+  },
+  {
+    "revision": "7fb23f3b56a834d9dcf615b5e7c78da8",
+    "url": "/TrustShare/static/media/bootstrap-icons.7fb23f3b.woff"
+  },
+  {
+    "revision": "e07b538aa51b6fa77f32828af21cb591",
+    "url": "/TrustShare/static/media/bootstrap-icons.e07b538a.woff2"
+  },
+  {
+    "revision": "44d2ead3b12748c151824be24cdc671d",
+    "url": "/TrustShare/index.html"
+  }
+];
